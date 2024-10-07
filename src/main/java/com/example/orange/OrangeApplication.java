8d57d42406834d3de54d91a7daf1d0b8e62f24ac
WassimB12 <Becheikh.wassim@esprit.tn>
2024-10-07 19:26:32 +0100

    prometheus updates

--- src/main/java/com/example/orange/OrangeApplication.java
+++ src/main/java/com/example/orange/OrangeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -21,6 +22,7 @@ import static java.time.LocalTime.now;
 @CrossOrigin(origins = "*")
 @EnableCaching
 @EnableScheduling
+@Async
 
 public class OrangeApplication {
 
